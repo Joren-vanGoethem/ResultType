577eb9249fb6ef0c7068e6ca47afcfa3bae99d37
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2025-07-01 19:50:56 +0200

    remove obsolete `ValidationKey` and refactor `Result` for improved error handling and consistency

--- JV.Utils/Result.cs
+++ JV.Utils/Result.cs
@@ -4,112 +4,111 @@ using System.Linq;
 
 namespace JV.Utils
 {
-    public class Result<TValue> : ResultType
+  public class Result<TValue> : ResultType
+  {
+    public TValue Value { get; }
+
+    private Result(TValue value, IEnumerable<ValidationMessage> validationMessages)
     {
-        public TValue Value { get; }
-
-        private Result(TValue value, IEnumerable<ValidationMessage> validationMessages)
-        {
-            Value = value;
-            ValidationMessages = validationMessages;
-        }
-
-        public static Result<TValue> Create(TValue value)
-            => new Result<TValue>(value, []);
-        
-        public static Result<TValue> Create(TValue value, IEnumerable<ValidationMessage> validationMessages)
-            => new Result<TValue>(value, validationMessages);
-
-        public Result<TValue> Merge(params Result[] results)
-        {
-            return new Result<TValue>(Value, ValidationMessages.Concat(results.SelectMany(r => r.ValidationMessages)));
-        }
-
-        public void Deconstruct(out bool isSuccessful, out IEnumerable<ValidationMessage> messages, out TValue value)
-        {
-            isSuccessful = IsSuccessful;
-            messages = ValidationMessages;
-            value = Value;
-        }
-
-        public void Deconstruct(out Result validationResult, out TValue value)
-        {
-            validationResult = Result.Create(ValidationMessages);
-            value = Value;
-        }
-
-        public Result<TValue> Merge(Result<TValue> result)
-        {
-            return new Result<TValue>(result.Value, ValidationMessages.Concat(result.ValidationMessages));
-        }
-
-        public static implicit operator Result<TValue>(Result result)
-        {
-            if (result.IsSuccessful)
-                throw new NotSupportedException(
-                    "Cannot implicitly convert empty result to successful result with value.");
-
-            // value is null here, converted from error result, exclamation mark to supress warning
-            return new Result<TValue>(default!, result.ValidationMessages); 
-        }
-
-        public static implicit operator Result<TValue>(TValue value)
-        {
-            return Create(value);
-        }
+      Value = value;
+      ValidationMessages = validationMessages;
     }
 
-    public class Result : ResultType
+    public static Result<TValue> Create(TValue value)
+      => new Result<TValue>(value, []);
+
+    public static Result<TValue> Create(TValue value, IEnumerable<ValidationMessage> validationMessages)
+      => new Result<TValue>(value, validationMessages);
+
+    public Result<TValue> Merge(params Result[] results)
     {
-        private Result(IEnumerable<ValidationMessage> validationMessages)
-        {
-            ValidationMessages = validationMessages;
-        }
-
-        private Result(ValidationMessage validationMessage)
-        {
-            ValidationMessages = new[] { validationMessage };
-        }
-
-        /// <summary>
-        /// Combines two results into one result
-        /// </summary>
-        public Result Merge(Result result)
-        {
-            return new Result(ValidationMessages.Concat(result.ValidationMessages));
-        }
-
-        public void Deconstruct(out bool isSuccessful, out IEnumerable<ValidationMessage> messages)
-        {
-            isSuccessful = IsSuccessful;
-            messages = ValidationMessages;
-        }
-
-        public static Result Create(IEnumerable<ValidationMessage> validationMessages) =>
-            new (validationMessages);
-        
-        public static Result<TValue> Create<TValue>(TValue value, IEnumerable<ValidationMessage> validationMessages) => 
-            Result<TValue>.Create(value, validationMessages);
-        
-
-        public static Result Ok() => new (Enumerable.Empty<ValidationMessage>());
-
-        public static Result<TValue> Ok<TValue>(TValue value) => Result<TValue>.Create(value);
-
-        public static Result Error(string translationKey, string[] parameters)
-            => new (ValidationMessage.CreateError(translationKey, parameters));
-        
-        public static Result Error(string translationKey)
-            => new (ValidationMessage.CreateError(translationKey));
-
-
-        public static Result Error(string translationKey, params object[] parameters)
-            => Error(translationKey, parameters.Select(p => p.ToString()).ToArray());
-
-        public static Result Error(Result result)
-            => new Result(result.ValidationMessages);
-        
-        public static Result Error(IEnumerable<ValidationMessage> validationMessages)
-            => new Result(validationMessages);
+      return new Result<TValue>(Value, ValidationMessages.Concat(results.SelectMany(r => r.ValidationMessages)));
     }
+
+    public void Deconstruct(out bool isSuccessful, out IEnumerable<ValidationMessage> messages, out TValue value)
+    {
+      isSuccessful = IsSuccessful;
+      messages = ValidationMessages;
+      value = Value;
+    }
+
+    public void Deconstruct(out Result validationResult, out TValue value)
+    {
+      validationResult = Result.Create(ValidationMessages);
+      value = Value;
+    }
+
+    public Result<TValue> Merge(Result<TValue> result)
+    {
+      return new Result<TValue>(result.Value, ValidationMessages.Concat(result.ValidationMessages));
+    }
+
+    public static implicit operator Result<TValue>(Result result)
+    {
+      if (result.IsSuccessful)
+        throw new NotSupportedException(
+          "Cannot implicitly convert empty result to successful result with value.");
+
+      // value is null here, converted from error result, exclamation mark to supress warning
+      return new Result<TValue>(default!, result.ValidationMessages);
+    }
+
+    public static implicit operator Result<TValue>(TValue value)
+    {
+      return Create(value);
+    }
+  }
+
+  public class Result : ResultType
+  {
+    private Result(IEnumerable<ValidationMessage> validationMessages)
+    {
+      ValidationMessages = validationMessages;
+    }
+
+    private Result(ValidationMessage validationMessage)
+    {
+      ValidationMessages = new[] { validationMessage };
+    }
+
+    /// <summary>
+    /// Combines two results into one result
+    /// </summary>
+    public Result Merge(Result result)
+    {
+      return new Result(ValidationMessages.Concat(result.ValidationMessages));
+    }
+
+    public void Deconstruct(out bool isSuccessful, out IEnumerable<ValidationMessage> messages)
+    {
+      isSuccessful = IsSuccessful;
+      messages = ValidationMessages;
+    }
+
+    public static Result Create(IEnumerable<ValidationMessage> validationMessages) =>
+      new(validationMessages);
+
+    public static Result<TValue> Create<TValue>(TValue value, IEnumerable<ValidationMessage> validationMessages) =>
+      Result<TValue>.Create(value, validationMessages);
+
+
+    public static Result Ok() => new(Enumerable.Empty<ValidationMessage>());
+
+    public static Result<TValue> Ok<TValue>(TValue value) => Result<TValue>.Create(value);
+
+    public static Result Error(TranslationKeyDefinition translationKey, object[] parameters)
+      => new(ValidationMessage.CreateError(translationKey, parameters));
+    
+    public static Result Error(TranslationKeyDefinition translationKey, object parameter)
+      => new(ValidationMessage.CreateError(translationKey, parameter));
+
+    public static Result Error(TranslationKeyDefinition translationKey)
+      => new(ValidationMessage.CreateError(translationKey));
+    
+    public static Result Error(Result result)
+      => new Result(result.ValidationMessages);
+
+    public static Result Error(IEnumerable<ValidationMessage> validationMessages)
+      => new Result(validationMessages);
+  }
 }
--- JV.Utils/ResultType.cs
+++ JV.Utils/ResultType.cs
@@ -3,20 +3,21 @@ using System.Linq;
 
 namespace JV.Utils
 {
-    public abstract class ResultType
+  public abstract class ResultType
+  {
+    public bool IsSuccessful => !ValidationMessages.Any();
+    public bool IsFailure => !IsSuccessful;
+    public IEnumerable<ValidationMessage> ValidationMessages { get; protected set; } = null!;
+
+    public override string ToString()
     {
-        public bool IsSuccessful => !ValidationMessages.Any();
-        public bool IsFailure => !IsSuccessful;
-        public IEnumerable<ValidationMessage> ValidationMessages { get; protected set; } = null!;
-
-        public override string ToString()
-        {
-            return string.Join(", ", ValidationMessages.Select(m => m.KeyDefinition != null ? m.KeyDefinition.Key : m.TranslationKey.ToString()));
-        }
-
-        public string ToStringWithParameters()
-        {
-            return string.Join(", ", ValidationMessages.Select(vm => vm.MapToErrorMessage()));
-        }
+      return string.Join(", ",
+        ValidationMessages.Select(m => m.KeyDefinition != null ? m.KeyDefinition.Key : m.TranslationKey.ToString()));
     }
+
+    public string ToStringWithParameters()
+    {
+      return string.Join(", ", ValidationMessages.Select(vm => vm.MapToErrorMessage()));
+    }
+  }
 }
